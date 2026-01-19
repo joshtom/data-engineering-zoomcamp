7c3767a926b4dda32681156da1120788f5f4a6c4
Joshua <joshuaolarjide@gmail.com>
2026-01-19 19:03:58 +0100

    terraform infra

--- docker-terraform/terraform/main.tf
+++ docker-terraform/terraform/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "google" {
-  credentials = "./keys/my-cred.json"
-  project     = "terraform-484615-bucket-29389283"
+  credentials = "/Users/macbookpro2020/Documents/projects/gcp-keys/keys/my-cred.json"
+  project     = "terraform-484615"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "auto-expiring-bucket"
+  name          = "terraform-484615-terra-bucket-new"
   location      = "US"
   force_destroy = true
 
